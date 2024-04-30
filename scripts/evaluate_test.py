502477eba643746bbd2cf563810a8645994c1c76
fmg <fabien.morgan@csem.ch>
2024-04-30 15:41:17 +0200

    Enhanced evalutaion so that it finds and evaluates all the test runns in a specified folder

--- scripts/evaluate_test.py
+++ scripts/evaluate_test.py
@@ -1,17 +1,33 @@
+import os
+import json
 import pandas as pd
 
-filename = "evaluation_of_prediction_mmsr_train_nc_SupportRange-100to100.csv"
+path = "evaluation/"
+summary_filename = 'evaluation_summary.csv'
+evaluation_data = []
 
-equation_prediction = pd.read_csv(filename)
+for filename in os.listdir(path):
+    if filename.endswith(".json"):
+        with open(os.path.join(path, filename), 'r') as json_file:
+            data = json.load(json_file)
 
-total_entries = len(equation_prediction)
+            csv_filename = data['csv_filename']
 
-match_sum = equation_prediction['Match Equation'].sum()
+            equation_prediction = pd.read_csv(os.path.join(path, csv_filename))
+            
+            total_entries = len(equation_prediction)
+            match_sum = equation_prediction['Match Equation'].sum()
+            match_percentage = (match_sum / total_entries) * 100
 
-match_percentage = (match_sum / total_entries) * 100
+            evaluation_data.append({
+                'Model': data['model'],
+                'Minimum Support': data['minimum_support'],
+                'Maximum Support': data['maximum_support'],
+                'Total Entries': total_entries,
+                'Match Sum': match_sum,
+                'Match Percentage': match_percentage
+            })
+            
+evaluation_df = pd.DataFrame(evaluation_data)
 
-print(f'Total Entries: {total_entries}')
-
-print(f'Match Sum: {match_sum}')
-
-print(f'Match Percentage: {match_percentage}%')
+evaluation_df.to_csv(os.path.join(path,summary_filename), index=False)           
