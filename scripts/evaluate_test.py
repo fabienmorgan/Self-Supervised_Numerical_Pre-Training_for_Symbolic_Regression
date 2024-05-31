b09782e50c0ae7048132d725620673a920205b64
fmg <fabien.morgan@csem.ch>
2024-05-31 15:55:50 +0200

    Created visualisations and made sure that all the data is complete and that there are no redudant entries

--- scripts/evaluate_test.py
+++ scripts/evaluate_test.py
@@ -18,6 +18,7 @@ for filename in os.listdir(path):
             total_entries = len(equation_prediction)
             match_sum = equation_prediction['Match Equation'].sum()
             match_percentage = (match_sum / total_entries) * 100
+            number_of_samples = equation_prediction['Number of Samples'].iloc[0] if equation_prediction['Number of Samples'].nunique() == 1 else float('nan')
 
             evaluation_data.append({
                 'Model': data['model'],
@@ -28,7 +29,8 @@ for filename in os.listdir(path):
                 'Maximum Support': data['maximum_support'],
                 'Total Entries': total_entries,
                 'Match Sum': match_sum,
-                'Match Percentage': match_percentage
+                'Match Percentage': match_percentage,
+                'Number of Samples': number_of_samples
             })
             
 evaluation_df = pd.DataFrame(evaluation_data).sort_values(by=['Model', 'Minimum Support'])
